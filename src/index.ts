fa9dc023b85dd27726a191838b590fba417c568d
Rodrigues <fr.costa95@gmail.com>
2023-06-27 09:31:48 -0300

    feat(cors): improve cors config

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,19 @@ import cors from 'cors';
 const PORT = process.env.PORT || 8080;
 const server = express();
 
-const options: cors.CorsOptions = {
-  origin: ['*']
-};
+// const options: cors.CorsOptions = {
+//   origin: ['*']
+// };
 
-server.use(cors(options));
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+// server.use(cors(options));
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
 
-//   server.use(cors());
+  server.use(cors());
 
-//   next();
-// });
+  next();
+});
 
 server.use(express.json());
 
