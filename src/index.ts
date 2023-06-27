b2352749d80791a2cae852c306e06e044f76fe1c
Rodrigues <fr.costa95@gmail.com>
2023-06-27 09:20:45 -0300

    chore(config): install and configure cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
 import { userCreate } from './controllers/signup.js';
 import 'dotenv/config'
+import cors from 'cors';
 
 const PORT = process.env.PORT || 8080;
 const server = express();
+
+const options: cors.CorsOptions = {
+  origin: ['*']
+};
+
+server.use(cors(options));
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+
+//   server.use(cors());
+
+//   next();
+// });
+
 server.use(express.json());
 
 server.get('/', (req, res) => res.send('Server running successful'));
